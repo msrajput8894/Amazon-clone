37d8419042beb25ba3194f26f6e406f148df0af1
mahendra rajput <msrajput8894@gmail.com>
2024-07-15 01:21:19 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,6 +79,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
