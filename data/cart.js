e63f73b24c07339289781097e3be8df9a65f381d
mahendra rajput <msrajput8894@gmail.com>
2024-07-15 18:13:21 +0530

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export const cart = [
+import { products } from "./products.js";
+
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -26,4 +28,16 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
