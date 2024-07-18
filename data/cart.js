55eb11ad28c6cccdde4bde7db185c3479d39b677
mahendra rajput <akashrajput1434@gmail.com>
2024-07-18 16:11:42 +0530

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,3 @@
-import { products } from "./products.js";
-
 export let cart = JSON.parse(localStorage.getItem("cart"));
 
 if (!cart) {
@@ -7,10 +5,12 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
+      deliveryOptionId: '1',
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
+      deliveryOptionId: '2',
     },
   ];
 }
@@ -34,6 +34,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: '1'
     });
   }
 
