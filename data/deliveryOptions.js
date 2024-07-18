50d1e952d4d48c3693c15e83cfe57c68765890fe
mahendra rajput <akashrajput1434@gmail.com>
2024-07-18 19:36:06 +0530

    Save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,3 +1,5 @@
+
+
 export const deliveryOptions = [{
     id: '1',
     deliveryDays: 7,
@@ -11,4 +13,17 @@ export const deliveryOptions = [{
     id: '3',
     deliveryDays: 1 ,
     priceCents:999
-}]
+}];
+
+
+export function getDeliveryOption(deliveryOptionId){
+    let deliveryOption;
+
+    deliveryOptions.forEach((option) => {
+      if (option.id === deliveryOptionId) {
+        deliveryOption = option;
+      }
+    });
+
+    return deliveryOption || deliveryOptions[0];
+}
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 
 const today = dayjs();
 const deliveryDate = today.add(7, "days");
@@ -14,23 +14,12 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-    let matchingProduct;
+    const matchingProduct = getProduct(productId);
 
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
-
+    
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
