4a0330116185aeecfb07f837133e4d5e366060ac
mahendra rajput <msrajput8894@gmail.com>
2024-07-26 15:53:46 +0530

    added feature to display the message on screen once the product is added to cart.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -52,7 +52,7 @@ function renderProductGrid() {
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
               <img src="images/icons/checkmark.png" />
               Added
             </div>
@@ -78,7 +78,7 @@ function renderProductGrid() {
 
   document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     button.addEventListener("click", () => {
-      const productId = button.dataset.productId;
+      const { productId } = button.dataset;
 
       //calling addToCart function
       addToCart(productId);
