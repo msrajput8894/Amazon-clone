55a5f62519851423792f1cfdf80c687790761a05
mahendra rajput <msrajput8894@gmail.com>
2024-07-28 15:26:37 +0530

    using MVC to render checkout header

---
+++ scripts/checkout/checkoutHeader.js
@@ -0,0 +1,39 @@
+import { cart } from "../../data/cart.js";
+
+export function renderCheckoutHeader() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  const checkoutHeaderHTML = `
+    <div class="header-content">
+        <div class="checkout-header-left-section">
+          <a href="index.html">
+            <img class="amazon-logo" src="images/amazon-logo.png" />
+            <img
+              class="amazon-mobile-logo"
+              src="images/amazon-mobile-logo.png"
+            />
+          </a>
+        </div>
+
+        <div class="checkout-header-middle-section">
+          Checkout (<a
+            class="return-to-home-link js-cart-item-quantity"
+            href="index.html"
+          >${cartQuantity}</a
+          >)
+        </div>
+
+        <div class="checkout-header-right-section">
+          <img src="images/icons/checkout-lock-icon.png" />
+        </div>
+      
+    </div>
+
+   
+  `;
+
+  document.querySelector(".js-checkout-header").innerHTML = checkoutHeaderHTML;
+}
