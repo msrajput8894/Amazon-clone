e2a5b599865423ebbfa48b721ae551ebf59e9d76
mahendra rajput <msrajput8894@gmail.com>
2024-07-30 11:15:35 +0530

    added search functionality to tracking page

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -33,62 +33,67 @@ function renderProductGrid() {
     });
   }
 
-  filteredProducts.forEach((product) => {
-    productsHTML += `
-      <div class="product-container">
-            <div class="product-image-container">
-              <img
-                class="product-image"
-                src="${product.image}"
-              />
-            </div>
+  // Display a message if no products matched the search criteria
+  if (filteredProducts.length === 0) {
+    productsHTML = `<div class="no-products-message">No products matched your search.</div>`;
+  } else {
+    filteredProducts.forEach((product) => {
+      productsHTML += `
+        <div class="product-container">
+              <div class="product-image-container">
+                <img
+                  class="product-image"
+                  src="${product.image}"
+                />
+              </div>
 
-            <div class="product-name limit-text-to-2-lines">
-              ${product.name}
-            </div>
+              <div class="product-name limit-text-to-2-lines">
+                ${product.name}
+              </div>
 
-            <div class="product-rating-container">
-              <img
-                class="product-rating-stars"
-                src="${product.getStarUrl()}"
-              />
-              <div class="product-rating-count link-primary">${
-                product.rating.count
-              }</div>
-            </div>
+              <div class="product-rating-container">
+                <img
+                  class="product-rating-stars"
+                  src="${product.getStarUrl()}"
+                />
+                <div class="product-rating-count link-primary">${
+                  product.rating.count
+                }</div>
+              </div>
 
-            <div class="product-price">${product.getPrice()}</div>
+              <div class="product-price">${product.getPrice()}</div>
 
-            <div class="product-quantity-container">
-              <select class= "js-quantity-selector-${product.id}">
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
+              <div class="product-quantity-container">
+                <select class= "js-quantity-selector-${product.id}">
+                  <option selected value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
 
-            ${product.extraInfoHTML()}
+              ${product.extraInfoHTML()}
 
-            <div class="product-spacer"></div>
+              <div class="product-spacer"></div>
 
-            <div class="added-to-cart js-added-to-cart-${product.id}">
-              <img src="images/icons/checkmark.png" />
-              Added
-            </div>
+              <div class="added-to-cart js-added-to-cart-${product.id}">
+                <img src="images/icons/checkmark.png" />
+                Added
+              </div>
 
-            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
-              product.id
-            }">Add to Cart</button>
-      </div>
-  `;
-  });
+              <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+                product.id
+              }">Add to Cart</button>
+        </div>
+    `;
+    });
+  }
 
   document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
